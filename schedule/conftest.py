02d67bbc3909c8f6a6c0065c13ff68ff2662d582
rashed <x.suwaidi@gmail.com>
2022-11-04 14:33:59 +0400

    initil commit

---
+++ schedule/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from schedule.users.models import User
+from schedule.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
